9948eff7abde6120eef1100ff40c531f3769afb6
sist186 <sist186@DESKTOP-N10JODP>
2020-02-11 17:55:38 +0900

    2020-02-11

---
+++ BoardProject/src/com/sist/board/BoardDetailServlet.java
@@ -0,0 +1,67 @@
+package com.sist.board;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.sist.dao.*;
+
+@WebServlet("/BoardDetailServlet")
+public class BoardDetailServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8"); // 브라우저에 html을 보내는데 한글로 읽어라
+		PrintWriter out=response.getWriter(); // 브라우저가 읽어가는 메모리 위치
+		
+		String no=request.getParameter("no");
+		// 오라클 => 데이터를 가지고 온다
+		BoardDAO dao=new BoardDAO();
+		BoardVO vo=dao.boardDetailData(Integer.parseInt(no));
+		
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<link rel=stylesheet href=\"css/table.css\">");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<center>");
+		out.println("<h1>내용보기</h1>");
+		
+		out.println("<table id=\"table_content\" width=700>");
+		out.println("<tr>");
+		out.println("<th width=20%>번호</th>");  // th는 가운데 정렬
+		out.println("<td width=30% align=center>"+vo.getNo()+"</td>"); // td는 왼쪽정렬
+		out.println("<th width=20%>작성일</th>"); 
+		out.println("<td width=30% align=center>"+vo.getRegdate()+"</td>"); 
+		out.println("</tr>");
+		
+		out.println("<tr>");
+		out.println("<th width=20%>이름</th>");  
+		out.println("<td width=30% align=center>"+vo.getName()+"</td>");
+		out.println("<th width=20%>조회수</th>"); 
+		out.println("<td width=30% align=center>"+vo.getHit()+"</td>"); 
+		out.println("</tr>");
+		
+		out.println("<tr>");
+		out.println("<th width=20%>제목</th>");  
+		out.println("<td colspan=3>"+vo.getSubject()+"</td>");
+		out.println("</tr>");
+		
+		out.println("<tr>");
+		out.println("<td colspan=4 align=left valign=top height=200>"+vo.getContent()+"</td>");
+		out.println("</tr>");
+		
+		out.println("<tr>");
+		out.println("<td colspan=4 align=right>");
+		out.println("<a href=\"#\">수정</a>&nbsp;");
+		out.println("<a href=\"#\">삭제</a>&nbsp;");
+		out.println("<a href=\"BoardListServlet\">목록</a>");
+		out.println("</td>");
+		out.println("</tr>");
+		
+		out.println("</table>");
+	}
+
+}
---
+++ BoardProject/src/com/sist/board/BoardListServlet.java
@@ -0,0 +1,118 @@
+package com.sist.board;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.sist.dao.*;
+import java.util.*;
+
+@WebServlet("/BoardListServlet")
+public class BoardListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/*
+		 *     http://localhost/BoardProject/BoardListServlet
+		 *     =============================
+		 *        root => WebContent
+		 */
+		
+		// request: 요청: 사용자한테 값 보내달라고 하는 것. 즉, 사용자가 보내주는 값을 받을 때 사용. 
+		// response: 응답: 사용자한테 응답할 때 사용.  ex) HTML/XML을 만들어서 사용자에게 전송할 때 사용. 
+		response.setContentType("text/html; charset=UTF-8");
+		// setContentType: HTML로 보낼거냐 XML로 보낼거냐 컨텐트타입을 알려줌.
+		PrintWriter out=response.getWriter(); 
+		// 어디에다가 저장할지. 브라우저에서 읽어가는 위치.  
+		
+		// 사용자가 요청한 페이지를 받는다
+		String strPage=request.getParameter("page");
+		if(strPage==null)
+			strPage="1";
+		int curPage=Integer.parseInt(strPage);
+		
+		BoardDAO dao=new BoardDAO();
+		ArrayList<BoardVO> list=dao.boardListData(curPage);
+		
+		// 총페이지
+		int totalPage=dao.boardTotalPage();
+		
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<link rel=stylesheet href=\"css/table.css\">");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<center>");
+		out.println("<h1>자유게시판</h1>");
+		
+		out.println("<table id=\"table_content\" width=700>");
+		out.println("<tr>");
+		out.println("<td align=left>");
+		out.println("<a href=\"BoardInsert\">새글</a>");
+		out.println("</td>");
+		out.println("</tr>");
+		out.println("</table>");
+		
+		out.println("<table id=\"table_content\" width=700>");
+		out.println("<tr>");
+		out.println("<th width=10%>번호</th>");
+		out.println("<th width=45%>제목</th>");
+		out.println("<th width=15%>이름</th>");
+		out.println("<th width=20%>작성일</th>");
+		out.println("<th width=10%>조회수</th>");
+		out.println("</tr>");
+		// 출력 위치
+		for(BoardVO vo:list) //VO에서 10번 돌리게 해놨음 
+		{
+			out.println("<tr class=dataTr>");
+			out.println("<td width=10% align=center>"+vo.getNo()+"</td>");
+			out.println("<td width=45% align=left>");
+			out.println("<a href=BoardDetailServlet?no="+vo.getNo()+">");
+			out.println(vo.getSubject()+"</a>");
+			out.println("</td>");
+			out.println("<td width=15% align=center>"+vo.getName()+"</td>");
+			out.println("<td width=20% align=center>"+vo.getRegdate()+"</td>");
+			out.println("<td width=10% align=center>"+vo.getHit()+"</td>");
+			out.println("</tr>");
+		}
+		out.println("</table>");
+		
+		out.println("<table id=\"table_content\" width=700>");
+		out.println("<tr>");
+		out.println("<td align=left>");
+		out.println("Search:");
+		out.println("<select>");
+		out.println("<option>이름</option>");
+		out.println("<option>제목</option>");
+		out.println("<option>내용</option>");
+		out.println("</select>");
+		out.println("<input type=text size=15>");
+		out.println("<input type=button value=찾기>");
+		out.println("</td>");
+		out.println("<td align=right>");
+		/*
+		 *     특수문자
+		 *     	&nbsp; " "(공백)
+		 *     	&lt;   <
+		 *     	&gt;   >
+		 */
+		out.println("<a href=\"BoardListServlet?page="+(curPage>1?curPage-1:curPage)+"\">&lt;이전&gt;</a>");
+		out.println(curPage+" page / "+totalPage+" pages");
+		out.println("<a href=\"BoardListServlet?page="+(curPage<totalPage?curPage+1:curPage)+"\">&lt;다음&gt;</a>");
+		out.println("</td>");
+		out.println("</tr>");
+		out.println("</table>");
+		
+		out.println("</center>");
+		out.println("</body>");
+		out.println("</html>");
+		// 메모리에 저장된 애를 브라우저가 읽어가서 출력함 
+		// jsp에서 초록색으로 나오는 애들은 사실 앞에 out.println이 생략된 것.
+		// 복잡... ==> HTML 출력할 땐 Servlet 잘 안 씀. 보안을 요구하거나 할 때만 사용함. 
+		// 다만, Spring이 Servlet으로 되어 있으므로 잘 기억할 것. 
+		
+	}
+
+}
